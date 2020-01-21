e5eaefc936e3552ecc34a203b0305cc603ac597d
Jsparks1989 <jsparks19891@gmail.com>
2020-01-21 02:28:24 -0500

    adding python boto3 code for downloading, extracting, uploading build zip file to portfolio bucket

---
+++ lambda-function.py
@@ -0,0 +1,24 @@
+import boto3
+from io import BytesIO
+import zipfile
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('justin-sparks-react-portfolio')
+build_bucket = s3.Bucket('justin-sparks-react-portfolio-build')
+
+# Downloading the zip file to memory, not folder structure 
+portfolio_zip = BytesIO()
+build_bucket.download_fileobj('reactPortfolioBuild.zip', portfolio_zip)
+
+# extracting the files from the zip folder
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    # must iterate through myzip.namelist() to get each file name in zip
+    for nm in myzip.namelist():
+        # opening each file in iteration
+        obj = myzip.open(nm)
+        # after individual file is opened, uploading it to portfolio bucket
+        # upload_fileobj(the_object, the_file_name)
+        portfolio_bucket.upload_fileobj(obj, nm)
+        # setting each file in portfolio bucket to public-read
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
